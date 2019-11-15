0da5b93b7d2691a10ac96af3244bfb297c9d9d4b
Simon Peter Ojok <simonojok19@gmail.com>
2019-11-15 14:16:48 +0300

    finished testing

--- app/src/main/java/com/commonsware/empublite/EmPubLiteActivity.java
+++ app/src/main/java/com/commonsware/empublite/EmPubLiteActivity.java
@@ -9,6 +9,5 @@ public class EmPubLiteActivity extends Activity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
-    System.out.println("Hello world");
   }
 }
