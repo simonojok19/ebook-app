9a82ac0e01c77fae50d8a4c7f2f3264d696c8ee3
Simon Peter Ojok <simonojok19@gmail.com>
2020-01-26 23:40:15 +0300

    spent time without coding and forgot where i stoped

--- app/src/main/java/com/commonsware/empublite/EmPubLiteActivity.java
+++ app/src/main/java/com/commonsware/empublite/EmPubLiteActivity.java
@@ -2,6 +2,8 @@ package com.commonsware.empublite;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class EmPubLiteActivity extends Activity {
 
@@ -9,5 +11,24 @@ public class EmPubLiteActivity extends Activity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.options, menu);
+    return super.onCreateOptionsMenu(menu);
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+      case R.id.about:
+        return (true);
+
+      case R.id.help:
+        // some text
+        return (true);
+    }
+    return (super.onOptionsItemSelected(item));
   }
 }
